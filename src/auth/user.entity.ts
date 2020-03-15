ec48c8abf235d4d516ba1cee2618331da8ce9cec
FrameTee29 <teeraphat123@hotmail.com>
2020-03-16 01:31:00 +0700

    salt

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -12,5 +12,8 @@ export class User extends BaseEntity{
 
     @Column()
     password:string;
+
+    @Column()
+    salt:string;
 }
 
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,24 +2,25 @@ import { Repository, EntityRepository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export  class UserRepository extends Repository<User>{
     async signUp(authCredentialsDto:AuthCredentialsDto){
         const {username , password} = authCredentialsDto;
 
-        const exists =this.findOne({username})
 
-        if(exists){
-            
-        }
+
+
 
         const user =new User();
         user.username = username;
-        user.password = password;
+        user.salt=await bcrypt.genSalt()
+        user.password = await this.hashPassword(password,user.salt);
 
+        console.log(user.password);
         try{
-            await user.save();
+            // await user.save();
         }
         catch(error){
             if(error.code === "ER_DUP_ENTRY"){
@@ -29,6 +30,10 @@ export  class UserRepository extends Repository<User>{
                 throw new InternalServerErrorException();
             }
         }
+    }
+
+    private async hashPassword(password:string,salt:string){
+        return bcrypt.hash(password,salt);
     }
 
 }
