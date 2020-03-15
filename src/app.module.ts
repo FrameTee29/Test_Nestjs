2c62590532f1e24494aae0de6ab55ccf50706249
FrameTee29 <teeraphat123@hotmail.com>
2020-03-15 23:10:23 +0700

    Create folder Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProfileModule } from './profile/profile.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig),
-    ProfileModule],
+    ProfileModule,
+    AuthModule],
 })
 export class AppModule {}
