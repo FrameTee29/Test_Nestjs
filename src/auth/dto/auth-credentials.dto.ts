b9a03206361ef76ac98953ad86c8a40aec2c2ec3
FrameTee29 <teeraphat123@hotmail.com>
2020-03-16 01:20:45 +0700

    try catach

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -7,8 +7,8 @@ export class AuthCredentialsDto {
     username:string;
 
     @IsString()
-    @MinLength(8)
-    @MaxLength(30)
+    @MinLength(6)
+    @MaxLength(20)
     @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z)(?=.*[a-z]).*$/,{message:'password too weak'})
     password:string;
 
