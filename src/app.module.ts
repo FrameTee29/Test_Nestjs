e9291106ed760b63a666d1f3d2b89675bc3622f5
FrameTee29 <teeraphat123@hotmail.com>
2020-03-15 17:04:57 +0700

    connect Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ProfileModule } from './profile/profile.module';
+import { DatabaseModule } from './database/database.module';
+import { Database } from './database';
 
 @Module({
 
 
 
-  imports: [ProfileModule]
+  imports: [ProfileModule, DatabaseModule],
+
+
+
+  providers: [Database]
 })
 export class AppModule {}
--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   controllers: [ProfileController],
-  providers: [ProfileService]
+  providers: [ProfileService],
+  imports: [DatabaseModule]
 })
 export class ProfileModule {}
